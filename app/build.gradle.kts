e33f216af42fca8115f0ad96f971d93c1219e0e1
NaveedQamar1 <navidqamar97@gmail.com>
2026-09-24 23:49:22 +0500

    Build Paisa Laya Android app

---
+++ app/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.paisalaya.app"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.paisalaya.app"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
