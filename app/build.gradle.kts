288b5b316806c7dffc19d5792b9a1fe606c63a2a
NaveedQamar1 <navidqamar97@gmail.com>
2026-09-25 17:07:56 +0500

    Add Google authorization dependency and bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
 }
 
 android {
+    // Google AuthorizationClient is used for private Google Drive app-data sync.
+
     namespace = "com.paisalaya.app"
     compileSdk = 35
 
@@ -10,8 +12,8 @@ android {
         applicationId = "com.paisalaya.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
     }
 
     buildTypes {
@@ -29,4 +31,9 @@ java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
     }
+}
+
+
+dependencies {
+    implementation("com.google.android.gms:play-services-auth:21.5.0")
 }
